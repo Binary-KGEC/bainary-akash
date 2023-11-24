a9001a57c474eaabe0e675f1ca9c7c99cd9a6e1a
shahank42 <shounakghosh42@gmail.com>
2023-11-24 20:59:48 +0530

    chore: added helpful stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
   return (
-    <main>
-      Binary Website
-    </main>
+    <>
+      <MaxWidthWrapper>
+        Binary Website
+      </MaxWidthWrapper>
+    </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+const Navbar = () => {
+  return (
+    <div>
+      <header>
+        <MaxWidthWrapper>
+          Navbar
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
