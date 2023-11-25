5aeef9ae3707a10e270dd0f7008cd3f56d844d42
imdebamrita <imdebamrita@gmail.com>
2023-11-25 22:36:04 +0530

    FAQ section is completed(dummy content), sponsors & prizes section initiated

--- src/components/Prizes.tsx
+++ src/components/Prizes.tsx
@@ -1,11 +1,23 @@
 import PageSection from "./PageSection";
 
 const Prizes = () => {
-	return (
-		<PageSection>
-			<p className="font-pixelate text-white">BINARY</p>
-		</PageSection>
-	);
-}
+  return (
+    <PageSection>
+      <div
+        id="prizes_section"
+        className="flex flex-col mb-20 my-5 sm:mb-20 justify-center items-center text-white"
+      >
+        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <h2 className="text-4xl text-center font-bold my-16 sm:mt-24 mb-8">
+            Prizes
+          </h2>
+          <div className="m-5 lg:mx-64 p-3">
+            <p>Coming Soon...</p>
+          </div>
+        </div>
+      </div>
+    </PageSection>
+  );
+};
 
 export default Prizes;
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,35 +1,61 @@
 export const items = [
-    {
-      name: "Home",
-      link: "#",
-    },
-    {
-      name: "About",
-      link: "#",
-    },
-    {
-      name: "Timeline",
-      link: "#",
-    },
-    {
-      name: "Tracks",
-      link: "#",
-    },
-    {
-      name: "Prizes",
-      link: "#",
-    },
-    {
-      name: "Mentors",
-      link: "#",
-    },
-    {
-      name: "Sponsors",
-      link: "#",
-    },
-    {
-      name: "FAQs",
-      link: "#",
-    },
-  ];
-  
+  {
+    name: "Home",
+    link: "#",
+  },
+  {
+    name: "About",
+    link: "#",
+  },
+  {
+    name: "Timeline",
+    link: "#",
+  },
+  {
+    name: "Tracks",
+    link: "#",
+  },
+  {
+    name: "Prizes",
+    link: "#prizes_section",
+  },
+  {
+    name: "Mentors",
+    link: "#",
+  },
+  {
+    name: "Sponsors",
+    link: "#sponsors_section",
+  },
+  {
+    name: "FAQs",
+    link: "#faq_section",
+  },
+];
+
+export const faqItems = [
+  {
+    question: "What is Binary?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, expedita quo voluptatem quis commodi consectetur corrupti consequuntur dicta cupiditate. Voluptatibus culpa",
+  },
+  {
+    question: "Who can participate in Binary?",
+    answer:
+      "Lorem ipsum dolor xpedita quo voluptatem quis ta cupiditate. Voluptatibus culpa",
+  },
+  {
+    question: "Is it offline?",
+    answer:
+      "Lorem ipsum dolor si commodi consectetur corrupti consequuntur dicta cupiditate. Voluptatibus culpa",
+  },
+  {
+    question: "What is the location?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elitetur corrupti consequuntur dicta cupiditate. Voluptatibus culpa",
+  },
+  {
+    question: "What is timeline?",
+    answer: "Lorem ipsum dolor sit amet consectetur adipisicing ulpa",
+  },
+];
