74bb8c6a5fa2bfbbd8731899f54fa3a03f30dd5b
akashbiswas7797 <abworkhouse01@gmail.com>
2023-12-30 10:09:53 +0530

    hh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "./fix.css";
 import { cn } from "@/lib/utils";
-import Navbar from "@/components/Navbar";
+
 import localFont from 'next/font/local';
 import MatrixRain from "@/components/MatrixRain";
 
